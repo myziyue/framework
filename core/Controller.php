c5628ec16af91056b52652887837620095fea63f
evan2884 <evan2884@gmail.com>
2017-03-02 18:28:19 +0800

    web端架构初始化

---
+++ core/Controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Bi Zhiming <evan2884@gmail.com>
+ * @created 2017/3/2  下午6:25
+ * @since 1.0
+ */
+
+namespace ziyue\core;
+
+
+class Controller extends Object
+{
+
+}
---
+++ web/Request.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Bi Zhiming <evan2884@gmail.com>
+ * @created 2017/3/2  下午6:26
+ * @since 1.0
+ */
+
+namespace ziyue\web;
+
+use ziyue\core\Object;
+
+class Request extends Object
+{
+    
+}
