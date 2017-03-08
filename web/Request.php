97909fb38fedff7cb18c4efe2be3e849f2d0243b
evan2884 <evan2884@gmail.com>
2017-03-08 11:38:56 +0800

    站点维护提示信息

--- web/Request.php
+++ web/Request.php
@@ -130,4 +130,54 @@ class Request extends Object
         return ((isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS']))) ||
             (isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'] ))) ? true : false;
     }
+
+    /**
+     * @return array|false|null|string
+     */
+    public function getClientIp()
+    {
+        static $realip = NULL;
+
+        if ($realip !== NULL) {
+            return $realip;
+        }
+
+        if (isset($_SERVER)) {
+            if (isset($_SERVER ['HTTP_X_FORWARDED_FOR'])) {
+                $arr = explode(',', $_SERVER ['HTTP_X_FORWARDED_FOR']);
+
+                /* 取X-Forwarded-For中第一个非unknown的有效IP字符串 */
+                foreach ($arr as $ip) {
+                    $ip = trim($ip);
+
+                    if ($ip != 'unknown') {
+                        $realip = $ip;
+
+                        break;
+                    }
+                }
+            } elseif (isset($_SERVER ['HTTP_CLIENT_IP'])) {
+                $realip = $_SERVER ['HTTP_CLIENT_IP'];
+            } else {
+                if (isset($_SERVER ['REMOTE_ADDR'])) {
+                    $realip = $_SERVER ['REMOTE_ADDR'];
+                } else {
+                    $realip = '0.0.0.0';
+                }
+            }
+        } else {
+            if (getenv('HTTP_X_FORWARDED_FOR')) {
+                $realip = getenv('HTTP_X_FORWARDED_FOR');
+            } elseif (getenv('HTTP_CLIENT_IP')) {
+                $realip = getenv('HTTP_CLIENT_IP');
+            } else {
+                $realip = getenv('REMOTE_ADDR');
+            }
+        }
+
+        preg_match("/[\d\.]{7,15}/", $realip, $onlineip);
+        $realip = !empty($onlineip [0]) ? $onlineip [0] : '0.0.0.0';
+
+        return $realip;
+    }
 }
