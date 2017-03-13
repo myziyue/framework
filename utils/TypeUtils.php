49da6e5532b65363687206c4fd0e68076223e999
evan2884 <evan2884@gmail.com>
2017-03-13 10:05:17 +0800

    工具类初始化

---
+++ utils/TypeUtils.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Bi Zhiming <evan2884@gmail.com>
+ * @created 2017/3/13  上午10:03
+ * @since 1.0
+ */
+
+namespace ziyue\utils;
+
+
+class TypeUtils
+{
+
+}
